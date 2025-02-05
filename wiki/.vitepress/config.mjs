7bbf6b1e238b3d332f9919a5f49b45fc8621cf0a
Sophron Ragozin <sophr.temin@ro.ru>
2025-02-05 12:56:56 +0500

    Update config.mjs

--- wiki/.vitepress/config.mjs
+++ wiki/.vitepress/config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "Larver Vanilla wiki",
+  title: "Larver Vanilla",
   description: "Вики",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
