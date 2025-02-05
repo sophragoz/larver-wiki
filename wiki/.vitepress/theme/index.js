04b9266f9d2ce0b61a68b5047a912ff108ee40ee
sophragoz <sophr.temin@ro.ru>
2025-02-05 20:37:25 +0500

    commit

--- wiki/.vitepress/theme/index.js
+++ wiki/.vitepress/theme/index.js
@@ -1,4 +1,3 @@
-// https://vitepress.dev/guide/custom-theme
 import { h } from 'vue'
 import DefaultTheme from 'vitepress/theme'
 import './style.css'
@@ -8,7 +7,6 @@ export default {
   extends: DefaultTheme,
   Layout: () => {
     return h(DefaultTheme.Layout, null, {
-      // https://vitepress.dev/guide/extending-default-theme#layout-slots
     })
   },
   enhanceApp({ app, router, siteData }) {
